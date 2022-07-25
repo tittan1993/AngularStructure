2fa1dbd4fe4e2e528481b20b44d505d2cbe6de1a
Alejandro gil olaya <gagil@bancolombia.com.co>
2022-07-25 15:07:39 -0500

    parallax and header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,10 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  redirectUser() {
+  location.href = "https://www.google.com/";
   }
 
 }
--- src/app/material/angular-meterial.module.ts
+++ src/app/material/angular-meterial.module.ts
@@ -3,20 +3,22 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from "@angular/material/icon";
-
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatMenuModule
   ],
   declarations: [],
   exports: [
     CommonModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule
   ]
 })
 export class AngularMaterialModule {}
